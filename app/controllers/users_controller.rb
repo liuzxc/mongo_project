5ca28fbd8ab54b8aa0108b98f2ae3bf9819589ec
liuzxc <lxq_102172@163.com>
2015-08-21 18:00:47 +0800

    fix bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to log_in, notice: 'User was successfully created.' }
+        format.html { redirect_to log_in_path, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
