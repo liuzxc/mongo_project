378cc3862cfa7306ac8e3cddb78c65c68a3f2e78
liuzxc <lxq_102172@163.com>
2015-09-06 11:35:41 +0800

    页面显示优化

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment
   include Mongoid::Document
   field :name, type: String
   field :content, type: String
+  field :created_at, type: Time, default: Time.now
 
   embedded_in :article, inverse_of: :comments
 end
